4bf5cf6efe5c957c53e2b9a268bcd779e9aad28a
TJ Trapp <tjtrapp@gmail.com>
2012-01-18 10:06:58 -0800

    first round of website.  shell, global css, etc...

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title>purlative</title>
+  <link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon">
+  <link rel="stylesheet" href="http://yui.yahooapis.com/3.4.1/build/cssreset/cssreset.css" type="text/css">
+  <link rel="stylesheet" type="text/css" href="/css/global.css">
+	<script type='text/javascript'>(function () { var a = ['header', 'nav', 'section', 'article', 'aside', 'footer', 'time']; for(var i=0,l=a.length;i<l;i++){ document.createElement(a[i]); } })()</script>
+</head>
+<body>  
+<header>
+  <img src="/img/logo_45x45.png">
+  <h1>Purlative</h1>
+  <h2>smarter, not harder.</h2>
+  <ul>
+    <li><a href="#work">work stuff</a></li>
+    <li><a href="#home">home stuff</a></li>
+  </ul>
+</header>
+<section class='content'>
+hi.  im currently coding the site.  hit refresh, who knows ya know :)
+</section>
+</body>
+</html>
