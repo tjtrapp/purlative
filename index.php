15e64c305c4c105412ebf2a1cb8e7e74cbf2f658
TJ Trapp <tjtrapp@gmail.com>
2012-01-18 11:41:51 -0800

    completed css for content items and tested in chrome, safari and webkit

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon">
   <link rel="stylesheet" href="http://yui.yahooapis.com/3.4.1/build/cssreset/cssreset.css" type="text/css">
   <link rel="stylesheet" type="text/css" href="/css/global.css">
+  <link rel="stylesheet" type="text/css" href="/css/content.css">
 	<script type='text/javascript'>(function () { var a = ['header', 'nav', 'section', 'article', 'aside', 'footer', 'time']; for(var i=0,l=a.length;i<l;i++){ document.createElement(a[i]); } })()</script>
 </head>
 <body>  
@@ -16,10 +17,76 @@
   <ul>
     <li><a href="#work">work stuff</a></li>
     <li><a href="#home">home stuff</a></li>
+    <li><a href="https://github.com/tjtrapp/purlative" target="_blank" alt="purlative on github">GitHub.com Repository</a></li>
   </ul>
 </header>
 <section class='content'>
-hi.  im currently coding the site.  hit refresh, who knows ya know :)
+  <div class="content-item">
+    <time>
+      <span class="post-time">11:30 am</span>
+      <span class="month">January</span>
+      <span class="day">18</span>
+    </time>
+    <article class="post">
+      <p class="caption">
+        Completed basic css for the content items.  tested this code in chrome, safari and ff... no ie since i dont have it on my mac.
+        Now that the basics are up, I'll push this to git and tweet it out.  
+      </p>
+      <div class="action-item">next steps after that are:
+        <ol>
+          <li>break 4 housework :/</li>
+          <li>start writing js code to flip b/t views</li>
+          <li>add some data storage mechanism</li>
+        </ol>
+      </div>
+    </article>
+  </div>
+
+  <div class="content-item">
+    <time>
+      <span class="post-time">10:30 am</span>
+      <span class="month">January</span>
+      <span class="day">18</span>
+    </time>
+    <article class="post">
+      <p class="caption">Now that the initial code is up on git, its time to make the css for the content items.</p>
+    </article>
+  </div>
+  <div class="content-item">
+    <time>
+      <span class="post-time">10:20 am</span>
+      <span class="month">January</span>
+      <span class="day">18</span>      
+    </time>
+    <article class="post">
+      <p class="caption">
+        Pushed my initial code for this site to Git for the world to see...
+      </p>
+      <p class="action-item">goto https://github.com/tjtrapp/purlative and check out my code!</p>
+    </article>
+  </div>
+  <div class="content-item">
+    <time>
+      <span class="post-time">10:00 am</span>
+      <span class="month">January</span>
+      <span class="day">18</span>
+    </time>
+    <article class="post">
+      <p class="caption">
+        I've completed the initial shell of the site.  I've added the header and styles, a reset from yui and a couple menu items.        
+      </p>      
+    </article>
+  </div>
+  <div class="content-item">
+    <time>
+      <span class="post-time">09:21 am</span>
+      <span class="month">January</span>
+      <span class="day">18</span>
+    </time>
+    <article class="post">
+      <p class="caption">hi.  im currently coding the site.  hit refresh, who knows ya know :)</p>
+    </article>
+  </div>
 </section>
 </body>
 </html>
