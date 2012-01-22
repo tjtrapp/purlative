9af425d617587a1e893de335b8a7475fffb93925
TJ Trapp <tjtrapp@gmail.com>
2012-01-22 08:45:21 -0800

    beachmint test code

---
+++ js/bm.js
@@ -0,0 +1,36 @@
+
+var Mint = {};  // top level namespace
+Mint.Abstract = {}; 
+
+Mint.Abstract.Vehicle = function () { }
+Mint.Abstract.Vehicle.prototype = (function () {
+    
+    // private members on the prototype
+    var name = "",
+        seats = 0,
+        wheels = 0;
+    
+    // methods to access the private data
+    return {
+        getSeats: function () { return seats; },
+        setSeats: function (val) { seats = val; },
+        getName: function () { return name; },
+        setName: function (val) { name = val; },
+        getWheels: function () { return wheels; },
+        setWheels: function (val) { wheels = val; }
+    }
+        
+})();
+
+Mint.Instances = {};
+Mint.Instances.Car = function () { 
+    this.setSeats(4);
+    this.setWheels(4);
+};
+Mint.Instances.Car.prototype = new Mint.Abstract.Vehicle();
+
+Mint.Instances.MotorCycle = function () {
+    this.setSeats(2);
+    this.setWheels(2);
+};
+Mint.Instances.MotorCycle.prototype = new Mint.Abstract.Vehicle();
